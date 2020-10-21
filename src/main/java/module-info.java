7ca89ff554b868aeecfe440a918b114d9cf2e5c6
VISTALL <vistall.valeriy@gmail.com>
2020-10-22 00:09:55 +0300

    migrate to jdk11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author VISTALL
+ * @since 2020-10-22
+ */
+module consulo.internal.dotnet.asm {
+    requires consulo.annotation;
+
+    exports consulo.internal.dotnet.asm;
+    exports consulo.internal.dotnet.asm.io;
+    exports consulo.internal.dotnet.asm.metadata;
+    exports consulo.internal.dotnet.asm.parse;
+    exports consulo.internal.dotnet.asm.signature;
+}
